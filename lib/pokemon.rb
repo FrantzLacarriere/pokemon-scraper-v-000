312d6333857045585c6dce6d39a0790c1a3ae2f3
Frantz Lacarriere <frantz@flatironschool.com>
2018-10-11 02:04:53 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_reader :name, :type, :db
   
-  def initialize :name, :type, :db 
+  def initialize name, type, db 
     @name = arg[:name]
     @type = arg[:type]
     @db = arg[:db]
